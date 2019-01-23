f409e261e36b2ea8769664de043a00264967373f
MoHo Khaleqi <habibsandyfan@gmail.com>
2019-01-23 03:35:56 +0000

    Server without graphql

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const axios = require('axios');
+
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+app.get('/launches', (req, res) => {
+  axios
+    .get('https://api.spacexdata.com/v3/launches')
+    .then(result => res.json(result.data))
+    .catch(error => res.send(error));
+});
+
+app.get('/launches/:flight_number', (req, res) => {
+  axios
+    .get(`https://api.spacexdata.com/v3/launches/${req.params.flight_number}`)
+    .then(result => res.json(result.data))
+    .catch(error => res.send(error));
+});
+
+app.get('/rockets', (req, res) => {
+  axios
+    .get('https://api.spacexdata.com/v3/rockets')
+    .then(result => res.json(result.data))
+    .catch(error => res.send(error));
+});
+
+app.get('/rockets/:rocket_id', (req, res) => {
+  axios
+    .get(`https://api.spacexdata.com/v3/rockets/${req.params.rocket_id}`)
+    .then(result => res.json(result.data))
+    .catch(error => res.send(error));
+});
+
+app.listen(PORT, () =>
+  console.log(`Server is running on http://localhost:${PORT}`)
+);
